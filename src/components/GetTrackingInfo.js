e602c7ce003f670c677fe2c759a880b3d4c8a2b6
codenm007 <n.majumder14@gmail.com>
2021-11-17 22:25:39 +0530

    added tracking info modal

--- src/components/GetTrackingInfo.js
+++ src/components/GetTrackingInfo.js
@@ -1,13 +1,38 @@
-import {Button,Modal,InputGroup,FormControl } from 'react-bootstrap';
+import {Button,Modal,InputGroup,FormControl,Badge } from 'react-bootstrap';
 import { useState, useEffect } from "react";
-const GetTrackingInfo = ({state}) =>{
+import isLoggedIn from "../functions/isLoggedIn";
 
-    const [show, setShow] = useState(state);
+const GetTrackingInfo = ({id,totalClicks}) =>{
+
+  const [Trackingdisabled , setTrackingdisabled] = useState(true);
+    const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+   
+  useEffect(()=>{
+    if(isLoggedIn()){
+      setTrackingdisabled(false);
+    }
+  },[])
+
+
+
+  const GetTrackingInfofun =(id) =>{
+    console.log(id,882);
+    setShow(true);
+    
+  }
 
     return (
+      <>
+      <Button 
+      variant="primary" 
+      onClick = {()=>GetTrackingInfofun(id)}
+       disabled = {Trackingdisabled}
+      className="float-end">
+        Clicks <Badge bg="secondary">{totalClicks}</Badge>
+        <span className="visually-hidden">Clicks</span>
+      </Button>
 <Modal
 show={show}
 onHide={handleClose}
@@ -15,7 +40,7 @@ backdrop="static"
 keyboard={false}
 >
 <Modal.Header closeButton>
-  <Modal.Title>Modal title</Modal.Title>
+  <Modal.Title>Modal title{id}</Modal.Title>
 </Modal.Header>
 <Modal.Body>
   I will not close if you click outside me. Don't even try to press
@@ -28,6 +53,7 @@ keyboard={false}
   <Button variant="primary">Understood</Button>
 </Modal.Footer>
 </Modal>
+</>
     )
 }
 
