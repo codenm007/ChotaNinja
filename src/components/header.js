d3ab425a0966299cf3d08e6f602b777b2edb8c1e
codenm007 <n.majumder14@gmail.com>
2021-11-18 16:24:51 +0530

    header update

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,6 @@ const Header = () => {
     <Container>
     <Navbar.Brand href="/"><img src = {ninjapic} width="40px" height="40px" className = "mx-2"/><span style ={{fontSize:"20px",fontWeight:"bold"}}>Chota.ninja </span> <span className="px-2" style={{fontSize:"12px"}}> Beta v1.0</span></Navbar.Brand>
     <Nav className="ml-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="/login">
         {isLoggedIn() ?(
         <NavDropdown
